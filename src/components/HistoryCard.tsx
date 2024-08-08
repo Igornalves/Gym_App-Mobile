85b297e2433cfd5f9a677efbc79386e4a2e02b9c
igor_alves <igornalves08@gmail.com>
2024-08-08 09:07:49 -0300

    colocando novas parte no history e aprendedno novos conceitos

--- src/components/HistoryCard.tsx
+++ src/components/HistoryCard.tsx
@@ -1,14 +1,47 @@
 import React from 'react';
-import { Heading, HStack, VStack } from '@gluestack-ui/themed';
+import { Heading, HStack, Text, VStack } from '@gluestack-ui/themed';
+import { THEME } from 'src/global/theme/intex';
 
 export default function HistoryCard() {
   return (
-    <HStack>
-        <VStack>
-            <Heading>
-                
-            </Heading>
-        </VStack>
+    <HStack
+      width="auto"
+      marginBottom={15}
+      padding={17}
+      borderRadius={10}
+      flexDirection='row'
+      alignItems='center'
+      justifyContent='space-between'
+      backgroundColor={THEME.colors.gray[600]}
+    >
+      <VStack 
+        marginRight={10}
+      >
+          <Heading
+            marginBottom={0}
+            marginTop={0}
+            color='white'
+            fontSize={THEME.fontSizes.md}
+            textTransform='capitalize'
+          >
+            Costas
+          </Heading>
+
+          <Text 
+            color={THEME.colors.gray[100]}
+            fontSize={THEME.fontSizes.lg}
+            numberOfLines={1}
+          >
+            Puxada frontal
+          </Text>
+      </VStack>
+
+      <Text 
+        color={THEME.colors.gray[300]}
+        fontSize={THEME.fontSizes.md}
+      >
+        08:56
+      </Text>
 
     </HStack>
   );
--- src/screens/History/intex.tsx
+++ src/screens/History/intex.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
-import { VStack } from '@gluestack-ui/themed'
+import React, { useState } from 'react';
+import { Heading, Text, VStack } from '@gluestack-ui/themed'
 import ScreenHeader from '@components/ScreenHeader';
 import HistoryCard from '@components/HistoryCard';
+import { SectionList } from 'react-native';
+import { THEME } from 'src/global/theme/intex';
 
 export default function History() {
+
+  const [exercises, setExercises] = useState([
+    {
+      title: '26.08.22',
+      data: ['Puxada frontal', 'Remada unilateral'],
+    },
+    {
+      title: '27.08.22',
+      data: ['Puxada frontal'],
+    },
+  ]);
+
   return (
     <VStack 
       flex={1}
@@ -12,11 +26,37 @@ export default function History() {
       //   justifyContent: 'center'
       // }}
     >
-        <ScreenHeader 
-          title='Histórico de Exercícios'
-        />
+      <ScreenHeader 
+        title='Histórico de Exercícios'
+      />
 
-        <HistoryCard/>
+      <SectionList
+        sections={exercises}
+        keyExtractor={item => item}     
+        renderItem={({ item }) => (
+          <HistoryCard/>
+        )}   
+        renderSectionHeader={({ section }) => (
+          <Heading
+            marginBottom={8}
+            marginTop={25}
+            color={THEME.colors.gray[200]}
+            fontSize={THEME.fontSizes.md}
+          >
+            {section.title}
+          </Heading>
+        )}
+        style={{ paddingHorizontal: 20 }}
+        contentContainerStyle={exercises.length === 0 && {flex: 1, justifyContent: 'center', alignItems: 'center'}}
+        ListEmptyComponent={() => (
+            <Text
+              color={THEME.colors.gray[100]}
+            >
+              Nao ha exercicios registrados ainda. {'\n     '}
+              Vamos fazer exercicios Hoje ?
+            </Text>
+        )}
+      />
     </VStack>
   );
 }
